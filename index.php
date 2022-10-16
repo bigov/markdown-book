9dd95feadd0c86f26394708bd476e9ded07b6649
bigov <bigov@yandex.com>
2022-10-16 10:26:25 +0900

    dev pause

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 
 const MD_DIR = 'data';        // папка базы данных по-умолчанию
 $file_index  = 'dirinfo.md';  // индекс по-умолчанию
-$tpls = 'assets/';            // папка шаблонов
 
+$tpls = 'assets' . DIRECTORY_SEPARATOR;            // папка шаблонов
 $page = 'page.tpl';
 $column = 'column.tpl';
 $footer = 'footer.tpl';
@@ -16,7 +16,6 @@ if ($_SERVER['REQUEST_URI'] == '/')
   exit;
 }
 
-
 require_once 'sys/tools.php';
 //require_once 'sys/php-markdown/Michelf/Markdown.inc.php';
 require_once 'sys/php-markdown/Michelf/MarkdownExtra.inc.php';
@@ -26,38 +25,50 @@ require_once 'sys/tree.php';
 use Michelf\MarkdownExtra;
 $PAD = new mdb\pad();
 
-// Если принят запрос с новым текстом, то записать и вернуться к файлу
+//print_html_page($PAD);
+
+// Если получен запрос с новым текстом, то записать и вернуться к файлу
 if (isset($_POST) and array_key_exists('editor', $_POST))
 {
     file_put_contents($PAD->fpath_fs, $_POST['editor']);
     header("Location: " . $_SERVER['SCRIPT_NAME']);
 }
 
-print (file_get_contents($tpls . "page_header.tpl"));
 
 if(is_null($PAD->err))
 {
-    if(is_file($PAD->fpath_fs))
+  if(is_file($PAD->fpath_fs))
+  {
+      $page_content =
+          MarkdownExtra::defaultTransform(
+              file_get_contents($PAD->fpath_fs)
+          );
+  }
+  elseif(is_file($PAD->fpath_fs . $file_index))
+  {
+    header("Location: " . $_SERVER['SCRIPT_NAME'] . '/' . $file_index);
+  }
+  else
+  {
+    $page_content = "<h1>Список файлов</h1>";
+    if(count($PAD->tree->ar_current)>0)
     {
-        $page_content = MarkdownExtra::defaultTransform(file_get_contents($PAD->fpath_fs));
+      foreach($PAD->tree->ar_current as $i => $v)
+      {
+          $page_content .= "<div class=\"side-menu\"><a href=\"$v\">$i</a></div>\n";
+      }
     }
-    else
-    {
-        $page_content = "<h1>Список файлов</h1>";
-        if(count($PAD->tree->ar_current)>0)
-        {
-            foreach($PAD->tree->ar_current as $i => $v)
-            {
-                $page_content .= "<div class=\"side-menu\"><a href=\"$v\">$i</a></div>\n";
-            }
-        }
-    }
+  }
 }
 else
 {
-    $page_content = '<H2>' . $PAD->err . '</H2>';
-    $footer = 'footer_er.tpl';
+  $page_content = '<H2>' . $PAD->err . '</H2>';
+  $footer = 'footer_er.tpl';
 }
+
+
+print (file_get_contents($tpls . "page_header.tpl"));
+
 
 if (array_key_exists('QUERY_STRING', $_SERVER) and str_starts_with($_SERVER['QUERY_STRING'], 'edit'))
 {
@@ -73,24 +84,5 @@ print ($page_content);
 print (file_get_contents($tpls . $column));
 
 require_once("view/side_menu.php");
-//print_r($PAD->tree);
-/**
-print ('<ul id="menu">');
-print ("<li><a href='/".MD_DIR."/$file_index'>HOME</a></li>");
-foreach ($PAD->top_dir_list_url as $title => $url)
-{
-    print ("<li><a href='$url/$file_index'>$title</a></li>");
-}
-print ("</ul>");
- */
-
-print ('<ul id="menu">');
-foreach ($PAD->files_list as $title => $url)
-{
-    print ("<li><a href='$url'>$title</a></li>");
-}
-print ("</ul>");
-
-
 print (file_get_contents($tpls . $footer));
 
---
+++ sys/render.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Make HTML page content for client brouser
+ */
+
+function print_html_page($P)
+{
+}
