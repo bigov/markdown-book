3b3de119616bb27510fc04aa59d90b8007194765
Игорь Балезин <bigov@yandex.ru>
2022-11-09 14:18:23 +0900

    moved out php.exe

--- buildin-server-run.cmd
+++ buildin-server-run.cmd
@@ -4,13 +4,13 @@ SETLOCAL
 chcp 65001
 SET RUNDIR=%~dp0
 SET PATH=%windir%;%windir%\system32;%RUNDIR%;%RUNDIR%sys\php
+SET "WMDB=%1"
 ECHO.
 ECHO Starting development php-server
 ECHO For end process press "Ctrl+C"
 ECHO.
 START http://localhost:8888/
-::sys\php\php.exe -S localhost:8888 -c %RUNDIR%sys\php\php.ini -t %RUNDIR% sys\router.php
-sys\php\php.exe -S localhost:8888 -c %RUNDIR%sys\php\php.ini -d include_path=%RUNDIR% -t "%1" sys\router.php
+%PHPEXE% -S localhost:8888 -c %RUNDIR%sys\php.ini -d include_path=%RUNDIR% -t "%1" sys\router.php
 ENDLOCAL
 
 ECHO Don't forget to commit...
