614748a3d2551821e9311df60b475f9ecfe65863
bigov <bigov@yandex.com>
2023-01-14 13:19:49 +0900

    new icons

--- sys/config.php
+++ sys/config.php
@@ -1,8 +1,9 @@
 <?php
 define("DIR_INDEX", "dirinfo.md");                          // directory index
+define("WMDB", $_ENV["WMDB"]);                              // Каталог WiKi
+
 define("ICO_DIR", "<span class=\"icon\" style=\"color: green;\">&#128447;</span>"); // Иконка папки
-define("ICO_TXT", "<span class=\"icon\" style=\"font-weight: bold; color: green; margin-right: 0.25em;\">&#128462;</span>"); // Иконка текстового документа
+define("ICO_TXT", "<span class=\"icon\" style=\"font-weight: bold; color: green; margin-right: 0.1em;\">&#128441;</span>"); // Иконка текстового документа
 define("ICO_PIC", "<span class=\"icon\" style=\"color: green;\">&#128445;</span>"); // Иконка изображения
 define("ICO_NEW_DIR", "<span class=\"icon\">&#128448;</span>"); // Иконка новой папки
-define("WMDB", $_ENV["WMDB"]);                              // Каталог WiKi
 
