8aff14bb31ae0b518d7aadd33568799443b3af1b
bigov <bigov@yandex.com>
2023-03-18 11:09:54 +0900

    add auto commit

--- start.cmd
+++ start.cmd
@@ -7,35 +7,40 @@ IF NOT EXIST "%1" GOTO _ERR_PATH
 
 SETLOCAL
 chcp 65001
-SET RUNDIR=%~dp0
+SET "RUNDIR=%~dp0"
 SET "WMDB=%1"
+CHDIR /D "%1"
+git pull
+CHDIR /D "%RUNDIR%"
+
 ECHO.
 ECHO Starting development php-server
 ECHO For end process press "Ctrl+C"
 ECHO.
 START http://localhost:8888/
 %PHPEXE% -S localhost:8888 -c %RUNDIR%sys\php.ini -d include_path=%RUNDIR% -t "%1" sys\router.php
-ENDLOCAL
 
-ECHO Don't forget to commit...
-pause
+CHDIR /D "%1"
+git add .
+git commit -am "modify"
+git push
+ENDLOCAL
 GOTO _EXIT
 
 :_ERR_PATH
 echo Not exist path: %1
-pause
 GOTO _EXIT
 
 :_ERR_CALL
 echo ERROR: data folder path not specified. Call format:
 echo.
 echo %0 X:\path_for\_MD_Wiki_data_folder
-echo.
-pause
 GOTO _EXIT
 
 :_ERR_PHP
 echo ERROR: not found environment variable PHPEXE.
-pause
 
 :_EXIT
+ECHO.
+pause
+ECHO.
